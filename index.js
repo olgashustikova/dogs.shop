77cff418dfa2958f130aaf8f7bd1e629eec9ce2f
olgashustikova <olga.shustikova1986@gmail.com>
2022-01-21 13:38:31 -0500

    next and prev buttons

--- index.js
+++ index.js
@@ -2,41 +2,22 @@ load()
 
 var panels
 
+var dogsArray
+var start = 0
+
 function load() {
   var client = new XMLHttpRequest()
   client.open('GET', 'config.json')
   client.onreadystatechange = function () {
     if (client.readyState === 4) {
       if (client.status === 200 || client.status == 0) {
-        const dogsArray = JSON.parse(client.responseText)
+        dogsArray = JSON.parse(client.responseText)
 
         var rootElement = document.querySelector('#root')
-        // "background-image: url(PICTURE URL)"
-        for (let i = 0; i < dogsArray.length; i++) {
-          var temp = document.querySelector('#dogPreview')
-          var clon = temp.content.cloneNode(true)
-          var dogImageDiv = clon.querySelector('#dogImage')
-          dogImageDiv.setAttribute(
-            'style',
-            'background-image: url(' + dogsArray[i].photoMain + ')'
-          )
 
-          var nameImageP = clon.querySelector('#nameDog')
-          nameImageP.innerHTML = dogsArray[i].name
-          var clickMoreP = clon.querySelector('#clickMore')
-          clickMoreP.setAttribute('onclick', 'detailsClick(' + i + ')')
-          nameImageP.setAttribute('onclick', 'detailsClick(' + i + ')')
+        processingTemplateDogPreview(rootElement)
 
-          rootElement.appendChild(clon)
-        }
-
-        panels = document.querySelectorAll('.panel')
-        panels.forEach((panel) => {
-          panel.addEventListener('click', () => {
-            removeActiveClasses()
-            panel.classList.add('active')
-          })
-        })
+        processingPanelDogPreview()
       }
     }
   }
@@ -52,4 +33,60 @@ function removeActiveClasses() {
 function detailsClick(dogIndex) {
   localStorage.setItem('dogIndex', dogIndex)
   document.location = 'details.html'
+}
+
+function processingTemplateDogPreview(rootElement) {
+  for (let i = start; i < start + 5; i++) {
+    var temp = document.querySelector('#dogPreview')
+    var clon = temp.content.cloneNode(true)
+    var dogImageDiv = clon.querySelector('#dogImage')
+    dogImageDiv.setAttribute(
+      'style',
+      'background-image: url(' + dogsArray[i].photoMain + ')'
+    )
+
+    var nameImageP = clon.querySelector('#nameDog')
+    nameImageP.innerHTML = dogsArray[i].name
+    var clickMoreP = clon.querySelector('#clickMore')
+    clickMoreP.setAttribute('onclick', 'detailsClick(' + i + ')')
+    nameImageP.setAttribute('onclick', 'detailsClick(' + i + ')')
+
+    rootElement.appendChild(clon)
+  }
+}
+
+function processingPanelDogPreview() {
+  panels = document.querySelectorAll('.panel')
+  panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+      removeActiveClasses()
+      panel.classList.add('active')
+    })
+  })
+}
+
+function next() {
+  const dogsPanelsRoot = document.getElementById('root')
+  while (dogsPanelsRoot.firstChild) {
+    dogsPanelsRoot.removeChild(dogsPanelsRoot.lastChild)
+  }
+
+  start = start + 5
+  if (start > dogsArray.length - 5) {
+    start = dogsArray.length - 5
+  }
+  processingTemplateDogPreview(dogsPanelsRoot)
+}
+
+function previous() {
+  const dogsPanelsRoot = document.getElementById('root')
+  while (dogsPanelsRoot.firstChild) {
+    dogsPanelsRoot.removeChild(dogsPanelsRoot.lastChild)
+  }
+
+  start = start - 5
+  if (start < 0) {
+    start = 0
+  }
+  processingTemplateDogPreview(dogsPanelsRoot)
 }
